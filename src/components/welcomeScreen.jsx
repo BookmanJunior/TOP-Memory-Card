fd53810b056ff528ecbb8dac3cc12335e327f450
Azam Gulov <gulovazam@gmail.com>
2023-09-06 21:06:52 +0500

    Add WelcomeScreen component
    add DifficultyButton component

---
+++ src/components/welcomeScreen.jsx
@@ -0,0 +1,21 @@
+export default function WelcomeScreen({ cb, children }) {
+  return (
+    <div className="welcome-screen">
+      <p>Choose game mode: </p>
+      <div className="game-mode-btns">
+        <DifficultyButton title="Easy" onClick={() => cb("easy")} />
+        <DifficultyButton title="Medium" onClick={() => cb("medium")} />
+        <DifficultyButton title="Hard" onClick={() => cb("hard")} />
+      </div>
+      {children}
+    </div>
+  );
+}
+
+function DifficultyButton({ title, onClick }) {
+  return (
+    <button className="font-bold" onClick={onClick}>
+      {title}
+    </button>
+  );
+}
