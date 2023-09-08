c0277d2baf30b62275afab4e2fad6e28586b946c
Azam Gulov <gulovazam@gmail.com>
2023-09-08 16:21:28 +0500

    add card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,1 +1,9 @@
-export default function Card() {}
+export default function Card({ data, handleClick }) {
+  const cardTitle = data.title?.english ?? data.title?.romaji;
+  return (
+    <div className="card-container" onClick={() => handleClick(data.id)}>
+      <img src={data.coverImage.extraLarge} alt={cardTitle} />
+      <p>{cardTitle}</p>
+    </div>
+  );
+}
