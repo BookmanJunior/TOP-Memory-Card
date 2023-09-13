0813663249150af7012b227a74bd4e32af6af0fe
Azam Gulov <gulovazam@gmail.com>
2023-09-13 15:28:59 +0500

    add utility class
    show current active button

--- src/components/welcomeScreen.jsx
+++ src/components/welcomeScreen.jsx
@@ -1,20 +1,37 @@
-export default function WelcomeScreen({ cb, children }) {
+export default function WelcomeScreen({ cb, gameDifficulty, children }) {
   return (
     <div className="welcome-screen">
       <p>Choose game mode: </p>
       <div className="game-mode-btns flex-row">
-        <DifficultyButton title="Easy" onClick={() => cb("easy")} />
-        <DifficultyButton title="Medium" onClick={() => cb("medium")} />
-        <DifficultyButton title="Hard" onClick={() => cb("hard")} />
+        <DifficultyButton
+          gameDifficulty={gameDifficulty}
+          title="Easy"
+          onClick={() => cb("easy")}
+        />
+        <DifficultyButton
+          gameDifficulty={gameDifficulty}
+          title="Medium"
+          onClick={() => cb("medium")}
+        />
+        <DifficultyButton
+          gameDifficulty={gameDifficulty}
+          title="Hard"
+          onClick={() => cb("hard")}
+        />
       </div>
       {children}
     </div>
   );
 }
 
-function DifficultyButton({ title, onClick }) {
+function DifficultyButton({ title, onClick, gameDifficulty }) {
   return (
-    <button className="font-bold" onClick={onClick}>
+    <button
+      className={`font-bold hover-underline ${
+        gameDifficulty === title.toLowerCase() ? "active" : ""
+      }`}
+      onClick={onClick}
+    >
       {title}
     </button>
   );
