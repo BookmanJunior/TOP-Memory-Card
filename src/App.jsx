8442d3d1308a76977c159d6bd1ca55b1ee4f1ffb
Azam Gulov <gulovazam@gmail.com>
2023-09-18 20:58:36 +0500

    Remove fetch request from App component
    remove data state
    remove loading screen from App component
    remove redundant helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,18 @@
 import { useEffect, useState } from "react";
 import WelcomeScreen from "./components/welcomeScreen";
-import Card from "./components/card";
 import Modal from "./components/modal";
-import TopScreen from "./components/topScreen";
+import Cards from "./components/card";
+import Header from "./components/header";
 
 export default function App() {
   const [gameState, setGameState] = useState("welcome-screen");
   const [gameDifficulty, setGameDifficulty] = useState("easy");
   const [score, setScore] = useState(0);
   const [topScore, setTopScore] = useState(0);
-  const [data, setData] = useState();
   const [clickedCards, setClickedCards] = useState([]);
 
   const isWelcomeScreen = gameState === "welcome-screen";
   const isLoading = gameState === "loading";
-  const numberOfCards =
-    gameDifficulty === "easy" ? 6 : gameDifficulty === "medium" ? 9 : 12;
-  const cardsSize =
-    gameDifficulty === "easy" || gameDifficulty === "medium"
-      ? "large"
-      : "small";
-
-  const handleGameMode = (mode) => {
-    setGameDifficulty(mode);
-  };
-
-  const handleGameState = (state) => {
-    setGameState(state);
-  };
-  const randomNum = () => Math.floor(Math.random() * numberOfCards);
-
-  const shuffleCards = () => {
-    const shuffledData = [];
-    const doesNumExist = [];
-
-    for (let index = 0; index < numberOfCards; index++) {
-      let num = randomNum();
-      while (doesNumExist.includes(num)) {
-        num = randomNum();
-      }
-      doesNumExist.push(num);
-      shuffledData.push(data[num]);
-    }
-
-    return shuffledData;
-  };
 
   const handleCardClick = (id) => {
     if (gameState !== "game-on") {
@@ -58,18 +26,15 @@ export default function App() {
     }
     setClickedCards([...clickedCards, id]);
     setScore((prev) => prev + 1);
-    const shuffledData = shuffleCards();
-    setData(shuffledData);
   };
 
   const reset = () => {
     setScore(0);
-    setData([]);
     setClickedCards([]);
   };
 
   const handleQuit = () => {
-    handleGameState("welcome-screen");
+    setGameState("welcome-screen");
     reset();
   };
 
@@ -91,125 +56,45 @@ export default function App() {
   }, [score, topScore]);
 
   useEffect(() => {
+    const numberOfCards =
+      gameDifficulty === "easy" ? 6 : gameDifficulty === "medium" ? 9 : 12;
     if (score >= numberOfCards) {
       setGameState("game-won");
       setClickedCards([]);
     }
-  }, [score, numberOfCards]);
+  }, [score, gameDifficulty]);
 
-  useEffect(() => {
-    const randomScore = () => {
-      const scores = [75, 80, 82, 85, 87, 88, 89];
-      const randomScoreNum = Math.floor(Math.random() * scores.length);
-      return scores[randomScoreNum];
-    };
-
-    async function getCharacters(numOfItems, queryScore) {
-      // Here we define our query as a multi-line string
-      // Storing it in a separate .graphql/.gql file is also possible
-      const query = `
-  query ($page: Int, $perPage: Int, $averageScore_greater: Int) {
-    Page (page: $page, perPage: $perPage) {
-      pageInfo {
-        total
-        currentPage
-        lastPage
-        hasNextPage
-        perPage
-      }
-      media (averageScore_greater: $averageScore_greater, type: MANGA) {
-        id
-        title {
-          romaji
-          english
-        }
-        coverImage {
-          extraLarge
-          large
-        }
-    }
-  }
-    }
-  `;
-
-      // Define our query variables and values that will be used in the query request
-      const variables = {
-        averageScore_greater: queryScore,
-        page: 1,
-        perPage: numOfItems,
-      };
-
-      // Define the config we'll need for our Api request
-      const url = "https://graphql.anilist.co";
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          query,
-          variables,
-        }),
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const jsonData = await response.json();
-        const parsedData = jsonData.data.Page.media;
-        setData(parsedData);
-        setGameState("game-on");
-      } catch (error) {
-        setGameState("error");
-      }
-    }
-    if (isLoading) {
-      getCharacters(numberOfCards, randomScore());
-    }
-  }, [isLoading, gameDifficulty, numberOfCards]);
-
-  if (isLoading) {
+  if (isWelcomeScreen) {
     return (
-      <p className="loading-screen">
-        Game is Loading
-        <span>.</span>
-        <span>.</span>
-        <span>.</span>
-      </p>
+      <WelcomeScreen cb={setGameDifficulty} gameDifficulty={gameDifficulty}>
+        <button
+          className="hover-underline"
+          onClick={() => setGameState("loading")}
+        >
+          Start Game
+        </button>
+      </WelcomeScreen>
     );
   }
 
   return (
     <>
-      {isWelcomeScreen && (
-        <WelcomeScreen cb={handleGameMode} gameDifficulty={gameDifficulty}>
-          <button
-            className="hover-underline"
-            onClick={() => handleGameState("loading")}
-          >
-            Start Game
-          </button>
-        </WelcomeScreen>
-      )}
-      {!isWelcomeScreen && (
-        <>
-          <TopScreen
-            handleQuit={handleQuit}
-            score={score}
-            topScore={topScore}
-          />
-          <div className={`cards-container ${cardsSize}`}>
-            {data.map((item) => (
-              <Card key={item.id} data={item} handleClick={handleCardClick} />
-            ))}
-          </div>
-        </>
-      )}
+      <>
+        {!isLoading && (
+          <Header handleQuit={handleQuit} score={score} topScore={topScore} />
+        )}
+        <Cards
+          gameDifficulty={gameDifficulty}
+          handleClick={handleCardClick}
+          gameState={gameState}
+          setGameState={setGameState}
+        />
+      </>
       <Modal
         gameState={gameState}
         score={score}
         handleRestart={() => {
-          handleGameState("loading");
+          setGameState("loading");
           reset();
         }}
         handleKeepPlaying={handleKeepPlying}
