5b58bd90d3ccb1aa2645a18299286e123a5c2496
Azam Gulov <gulovazam@gmail.com>
2023-09-28 21:27:18 +0500

    Initial commit

---
+++ src/gameOptions.types.tsx
@@ -0,0 +1,9 @@
+export type gameStateOptions =
+  | "welcome-screen"
+  | "loading"
+  | "game-over"
+  | "game-won"
+  | "game-on"
+  | "error";
+
+export type gameDifficultyOptions = "easy" | "medium" | "hard";
