89c30d5ac8a0b1c2056b12edd40fc65e0e05beda
Azam Gulov <gulovazam@gmail.com>
2023-09-29 15:35:13 +0500

    Code refactor
    add custom hook
    handle error separately
    rearrange helper functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import { gameStateOptions, gameDifficultyOptions } from "./gameOptions.types";
+import {
+  gameStateOptions,
+  gameDifficultyOptions,
+  Data,
+} from "./gameOptions.types";
 import WelcomeScreen from "./components/welcomeScreen";
-import Modal from "./components/modal";
+import { Modal, ErrorModal } from "./components/modal";
 import Cards from "./components/card";
 import Header from "./components/header";
+import APIData from "./components/getData";
 
 export default function App() {
+  const [data, setData] = useState<Data[]>([]);
   const [gameState, setGameState] =
     useState<gameStateOptions>("welcome-screen");
   const [gameDifficulty, setGameDifficulty] =
@@ -13,60 +19,15 @@ export default function App() {
   const [score, setScore] = useState(0);
   const [topScore, setTopScore] = useState(0);
   const [clickedCards, setClickedCards] = useState<number[]>([]);
+  const { error, setError } = APIData({
+    gameState,
+    setGameState,
+    setData,
+    gameDifficulty,
+  });
 
   const isWelcomeScreen = gameState === "welcome-screen";
-  const isLoading = gameState === "loading";
-  const numberOfCards = {
-    easy: 6,
-    medium: 9,
-    hard: 15,
-  } as const;
-
-  const handleCardClick = (id: number) => {
-    if (gameState !== "game-on") {
-      return;
-    }
-
-    if (clickedCards.includes(id)) {
-      setClickedCards([]);
-      setGameState("game-over");
-      return;
-    }
-
-    const newScore = score + 1;
-    setClickedCards([...clickedCards, id]);
-    setScore(newScore);
-
-    if (newScore > topScore) {
-      setTopScore(newScore);
-    }
-
-    if (newScore >= numberOfCards[gameDifficulty]) {
-      setGameState("game-won");
-      setClickedCards([]);
-    }
-  };
-
-  const reset = () => {
-    setScore(0);
-    setClickedCards([]);
-  };
-
-  const handleQuit = () => {
-    setGameState("welcome-screen");
-    reset();
-  };
-
-  const handleKeepPlying = () => {
-    if (gameDifficulty === "easy") {
-      setGameDifficulty("medium");
-    } else if (gameDifficulty === "medium") {
-      setGameDifficulty("hard");
-    }
-
-    setGameState("loading");
-    reset();
-  };
+  const isGameLoading = gameState === "loading";
 
   if (isWelcomeScreen) {
     return (
@@ -81,17 +42,30 @@ export default function App() {
     );
   }
 
+  if (error) {
+    return (
+      <ErrorModal
+        handleRestart={() => {
+          setError(false);
+          setGameState("loading");
+        }}
+        handleExit={() => setGameState("welcome-screen")}
+      />
+    );
+  }
+
+  if (isGameLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <>
       <>
-        {!isLoading && (
-          <Header handleQuit={handleQuit} score={score} topScore={topScore} />
-        )}
+        <Header handleQuit={handleQuit} score={score} topScore={topScore} />
         <Cards
+          data={data}
           gameDifficulty={gameDifficulty}
           handleClick={handleCardClick}
-          gameState={gameState}
-          setGameState={setGameState}
         />
       </>
       <Modal
@@ -105,5 +79,73 @@ export default function App() {
         gameDifficulty={gameDifficulty}
       />
     </>
+  );
+
+  function handleCardClick(id: number) {
+    if (gameState !== "game-on") {
+      return;
+    }
+
+    if (clickedCards.includes(id)) {
+      setClickedCards([]);
+      setGameState("game-over");
+      return;
+    }
+
+    const newScore = score + 1;
+    setClickedCards([...clickedCards, id]);
+    setScore(newScore);
+    setData(shuffleArray([...data]));
+
+    if (newScore > topScore) {
+      setTopScore(newScore);
+    }
+
+    if (newScore >= data.length) {
+      setGameState("game-won");
+      setClickedCards([]);
+    }
+  }
+
+  function handleQuit() {
+    setGameState("welcome-screen");
+    reset();
+  }
+
+  function handleKeepPlying() {
+    if (gameDifficulty === "easy") {
+      setGameDifficulty("medium");
+    } else if (gameDifficulty === "medium") {
+      setGameDifficulty("hard");
+    }
+
+    setGameState("loading");
+    reset();
+  }
+
+  function reset() {
+    setScore(0);
+    setClickedCards([]);
+  }
+}
+
+// Durstenfeld shuffle algorithm
+function shuffleArray(array: Data[]): Data[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // eslint-disable-next-line no-param-reassign
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function LoadingScreen() {
+  return (
+    <p className="loading-screen">
+      Game is Loading
+      <span>.</span>
+      <span>.</span>
+      <span>.</span>
+    </p>
   );
 }
